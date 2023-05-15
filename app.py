6b6ce7a511dd8e05f019f7662b5793539c4e1f9a
Daniil <landan2001@mail.ru>
2023-05-15 17:17:49 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,74 @@
+# -*- coding: cp1251 -*-
+
+from extensions import Exchange, ExchangeException
+from config import *
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot(token)
+
+
+def message_info(message):
+    user_name = message.from_user.username
+    user_text = message.text
+    print(f'{user_name}: {user_text}')
+
+
+@bot.message_handler(commands=["Start"])
+def button_commands(message):
+    markup = types.ReplyKeyboardMarkup()
+    command1 = types.KeyboardButton('Доступная валюта')
+    command2 = types.KeyboardButton('Как конвертировать валюту')
+    markup.add(command1, command2)
+    bot.send_message(message.chat.id, "Привет, это бот для конвертации валют, воспользуйтесь кнопкой "
+                                      "'Доступная валюта' или напишите сообщение 'Доступная валюта', что бы получть "
+                                      "подробную информацию.", reply_markup=markup)
+    message_info(message)
+
+
+@bot.message_handler(content_types=["text"])
+def func_ex(message):
+    if message.text == 'Доступная валюта':
+        text = (f"Доступная валюта для конвертации:"
+                f"\n")
+        for i in nac_ex.keys():
+            text += f"\n{i} - {nac_ex[i]}"
+        text += ("\n"
+                 "\n"
+                 "Что бы начать конвертацию валюты воспользуйтесь кнопкой "
+                 "'Как конвертировать валюту'."
+                 "\nИли напишите сообщение 'Как конвертировать валюту'")
+        bot.reply_to(message, text)
+    elif message.text == 'Как конвертировать валюту':
+        text = ("Введите сообщение в формате:"
+                "\n"
+                "\n$ RUB EUR 50"
+                "\n"
+                "\nГде:"
+                "\n'$' - знак конвертации"
+                "\n'RUB' - переводимая валюта"
+                "\n'EUR' - валюта в которую вы хотите перевести"
+                "\n'50' - количество переводимой валюты")
+        bot.reply_to(message, text)
+    elif "$" in message.text:
+        try:
+            list_ex = message.text.split()
+            if (len(list_ex) == 4) and (list_ex[0] == "$"):
+                val1 = list_ex[1]
+                val2 = list_ex[2]
+                quantity = list_ex[3]
+                value = Exchange.get_exchange(val1, val2, quantity)
+                bot.reply_to(message, f"Цена {quantity} {val1.upper()} = {value} {val2.upper()}")
+            else:
+                text_error = ("Ошибка в написании команды конвертации валюты."
+                              "\nПроверьте корректность введённой формы.")
+                bot.reply_to(message, text_error)
+        except ExchangeException as e:
+            bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+
+    else:
+        bot.send_message(message.chat.id, "Напишите команду /Start для работы с ботом")
+        message_info(message)
+
+
+bot.polling()
---
+++ extensions.py
@@ -0,0 +1,37 @@
+import requests
+import json
+from config import *
+
+
+class ExchangeException(Exception):
+    pass
+
+
+class Exchange:
+    @staticmethod
+    def get_exchange(val1, val2, quantity):
+        if val1.upper() == val2.upper():
+            raise ExchangeException(f'Нельзя перевести одинаковые валюты {val2}.')
+        try:
+            quote_value1 = nac_ex[val1.upper()]
+        except KeyError:
+            raise ExchangeException(f'Не смог обработать валюту {val1}, проверьте список доступных валют.')
+
+        try:
+            quote_value2 = nac_ex[val2.upper()]
+        except KeyError:
+            raise ExchangeException(f'Не смог обработать валюту {val2}, проверьте список доступных валют.')
+        try:
+            quantity = float(quantity)
+        except ValueError or SyntaxError or NameError:
+            raise ExchangeException(f'Не смог обработать количество {quantity}.')
+        if quantity <= 0:
+            raise ExchangeException(f'Не смог обработать количество {quantity}, число должно быть больше 0')
+
+        response = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={val1.upper()}&tsyms={val2.upper()}")
+        response = response.content
+        ex_info = json.loads(response)
+        result = round(ex_info[val2.upper()] * float(quantity), 3)
+        return result
+
+
